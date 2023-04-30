aa417f40a58468b3445f90ca19925bfe054cdab8
tttamaki1 <130736956+tttamaki1@users.noreply.github.com>
2023-04-30 18:19:17 +0900

    first commit

--- app/controllers/open_ai_controller.rb
+++ app/controllers/open_ai_controller.rb
@@ -9,23 +9,75 @@
     def index
     end
   
-    def stream_openai_api
-      Async do |task|
-        client = OpenAI::Client.new(access_token: ENV['OPENAI_API_KEY'])
+    #def stream_openai_api
+    def create
+        @plan = Plan.new()
+        @plan.destination = params[:destination]
+        @plan.duration = params[:duration]
+        @plan.activity = params[:activity]
+        @plan.transportation = params[:transportation]
+        @plan.activity = params[:place_to_visit]
+    
+        prompt = "
+    
+        Create create a time table and a specific and travel plan in Japanese for the following information:\n
+        Location: Korea\n
+        Duration: 1 nights 2 days\n
+        Budget: 60,000 yen\n
+        Accommodation: Hotel\n
+        Activity: food tour\n
+        transportation: Taxi\n
+        When: April\n 
+        Where you live: Chiba\n
+        Place to visit: \n
+        Please start from the airport for overseas travel, and from the nearest station to the destination for domestic travel.
+        If planning on overseas travel, get to the airport 2 to 3 hours before the departure time\n
+        Specifiy what to do in the each destination so that we can imagin it but, exclude what to do at the hotel, the airport and returning the car \n
+        Pick up a lot of recommended local foods around there or on the way and choose some of it and include it in the plan\n
+        If you recommend the distant location, include the information how to get there and taking into account the required travel time between each destination\n
+        If you can't find the activity, don't try to include it\n
+        Include the recommended food with the sentense like the reccomendation is ~\n
+        Pick up a lot of recommended souvenir shops and choose some of it and include some souvenir shops at the end of the plan\n
+        Include the transportation method in the each plan, where to get on and get off\n
+        Don't include the sentense like returning home with memories in one's heart\n
+        Don't include the sentense like relaxing time\n
+        Don't rent a car yet if not nessesary on the first day of the plan\n
+        Don't recommend drinking during the day if renting a car\n
+        Respond only the itinerary under\n
+        Specify the time\n
+        Then, please break down the plan into a daily itinerary,\n
+         specifically, please format the response with the format like\n
+         日程：
+         day1:
+         12:00 - 那覇空港に到着、レンタカーを借りる
+         13:00 - ホテルへ向かいチェックイン
+         14:00 - レンタカーで国際通りを散策、お土産屋さんを見る
+         18:00 - 地元のレストラン「かりゆしそば」で沖縄そばを堪能
+         20:00 - 那覇市内の居酒屋「うりずん」で沖縄料理を楽しむ（ゴーヤチャンプルーやラフテーなど）
+        "    
+        Async do |task|
+        client = OpenAI::Client.new(access_token: "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")
         client.chat(
-          parameters: {
+            parameters: {
             model: "gpt-3.5-turbo", # Required.
             messages: [{ role: "user", content: "Who is Elon Musk"}], # Required.
             temperature: 0.7,
             stream: proc do |chunk, _bytesize|
-              task.async do
+                task.async do
                 ActionCable.server.broadcast('open_ai_channel', chunk.dig("choices", 0, "delta", "content"))
-              end
+                end
             end
-          })
+        })
       end
     end
 
+    def search
+        @plans = Plan.search(params[:keyword])
+    end    
 
+    private
+    def plan_params
+      params.require(:plan).permit(:destination, :duration, :budget, :activity, :transportation, :place_to_visit)
+    end
 
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get '/', to: 'open_ai#index'
+  root to: 'open_ai#index'
   get 'open_ai/stream_openai_api', to: 'open_ai#stream_openai_api'
+  get 'open_ai/search', to: 'open_ai#search'
   mount ActionCable.server => '/cable'
 end
