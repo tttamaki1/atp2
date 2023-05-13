b122c8a0970833b20b530429a9a71c288c5a18d1
tttamaki1 <130736956+tttamaki1@users.noreply.github.com>
2023-05-13 19:11:18 +0900

    テキストボックスの修正
    オートコンプリート入力時の背景色が白になってしまうのを修正

--- app/javascript/packs/dark_mode.js
+++ app/javascript/packs/dark_mode.js
@@ -2,22 +2,28 @@ document.addEventListener("DOMContentLoaded", () => {
   const toggleDarkMode = () => {
     document.body.classList.toggle("dark-mode");
     if (document.body.classList.contains("dark-mode")) {
-      darkModeButton.textContent = "light";
+      // darkModeButton.textContent = "light";
       localStorage.setItem("darkMode", "enabled");
+      modeIcon.remove("fa-solid fa-sun-bright")
+      modeIcon.add("fa-regular fa-moon")
     } else {
-      darkModeButton.textContent = "dark";
+      // darkModeButton.textContent = "dark";
       localStorage.setItem("darkMode", "disabled");
+      modeIcon.remove("fa-regular fa-moon")
+      modeIcon.add("fa-solid fa-sun-bright")
     }
   };
 
   const darkModeButton = document.getElementById("dark-mode-toggle");
   darkModeButton.addEventListener("click", toggleDarkMode);
-
+  const modeIcon = document.getElementsByClassName("mode-icon")[0];
 
  // ダークモード設定の読み込み
  const darkModeSetting = localStorage.getItem("darkMode");
  if (darkModeSetting === "enabled") {
    document.body.classList.add("dark-mode");
+   modeIcon.add("fa-solid fa-sun-bright")
+
  }
 
 });
