e3483bd948f8221d5e5b37f19c543c721b9b736b
tttamaki1 <130736956+tttamaki1@users.noreply.github.com>
2023-05-25 23:05:44 +0900

    7

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -7,13 +7,7 @@ module ApplicationCable
     protected
 
     def verify_user
-
       tab_session_id = request.params[:tab_session_id]
-      valid_user = tab_session_id.present?
-      
-      unless valid_user
-        reject_unauthorized_connection
-      end
     end
   end
 end
