00e9e161a933d4fb5b41c1af9b640e70f90fb114
tttamaki1 <130736956+tttamaki1@users.noreply.github.com>
2023-06-01 21:59:22 +0900

    1

--- app/javascript/channels/inspiration_guide_channel.js
+++ app/javascript/channels/inspiration_guide_channel.js
@@ -1,23 +1,25 @@
 import { consumer, tabSessionId } from "./consumer";
 
-consumer.subscriptions.create(
-  { channel: 'InspirationGuideChannel', tab_session_id: tabSessionId },
-  {
-  connected() {
-    console.log("Connected to InspirationGuideChannel:"+ tabSessionId);
+document.addEventListener("turbolinks:load", function() {
+  consumer.subscriptions.create(
+    { channel: 'InspirationGuideChannel', tab_session_id: tabSessionId },
+    {
+    connected() {
+      console.log("Connected to InspirationGuideChannel:"+ tabSessionId);
 
-  },
+    },
 
-  disconnected() {
-    console.log("Disconnected from InspirationGuideChannel:"+ tabSessionId);
-  },
+    disconnected() {
+      console.log("Disconnected from InspirationGuideChannel:"+ tabSessionId);
+    },
 
-  received(data) {
-    const response = document.getElementById("response");
-    if (response && data != null) {
-      const content = response.innerHTML  + data;
-      response.innerHTML  = content;
+    received(data) {
+      const response = document.getElementById("response");
+      if (response && data != null) {
+        const content = response.innerHTML  + data;
+        response.innerHTML  = content;
+      }
     }
-  }
 
+  });
 });
--- app/javascript/channels/second_channel.js
+++ app/javascript/channels/second_channel.js
@@ -2,41 +2,43 @@ import { consumer, tabSessionId } from "./consumer";
 import { marking } from '../packs/google_maps';
 // console.log('tabSessionId')
 // console.log(sessionStorage.getItem('tabSessionId'))
-consumer.subscriptions.create(
-  {
-    channel: 'SecondChannel',
-    tab_session_id: tabSessionId
-  },
-  {
-  chunk: '', // ここで this.chunk を初期化
-  
-  connected() {
-    console.log("Connected to SecondChannel"+ tabSessionId);
-  },
+document.addEventListener("turbolinks:load", function() {
+  consumer.subscriptions.create(
+    {
+      channel: 'SecondChannel',
+      tab_session_id: tabSessionId
+    },
+    {
+    chunk: '', // ここで this.chunk を初期化
+    
+    connected() {
+      console.log("Connected to SecondChannel:"+ tabSessionId);
+    },
 
-  disconnected() {
-    console.log("Disconnected from SecondChannel"+ tabSessionId);
-  },
+    disconnected() {
+      console.log("Disconnected from SecondChannel:"+ tabSessionId);
+    },
 
-  async received(data) {
-    await new Promise(async resolve => {
-      if (data != null) {
-        if (data.includes("<br>")) {
-          // 文字列が<br>の場合、それまでの文字列をkeywordに入れる
-          let keyword = this.chunk;
-          keyword = keyword.replace(/^\d+\.\s*/, '');
-          keyword = keyword.replace('&amp;', '&');
-          console.log(keyword);
+    async received(data) {
+      await new Promise(async resolve => {
+        if (data != null) {
+          if (data.includes("<br>")) {
+            // 文字列が<br>の場合、それまでの文字列をkeywordに入れる
+            let keyword = this.chunk;
+            keyword = keyword.replace(/^\d+\.\s*/, '');
+            keyword = keyword.replace('&amp;', '&');
+            console.log(keyword);
 
-          marking(keyword) //キーワードの取り出しをopen_ai_channelへ移行する
+            marking(keyword) //キーワードの取り出しをopen_ai_channelへ移行する
 
-          this.chunk = "";       
-        } else {
-          // データが<br>でない場合、chunkに追加
-          this.chunk += data;
+            this.chunk = "";       
+          } else {
+            // データが<br>でない場合、chunkに追加
+            this.chunk += data;
+          }
         }
-      }
-    });
-  },
-});
+      });
+    },
+  });
+});  
 
