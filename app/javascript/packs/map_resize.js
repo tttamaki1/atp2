de3bf116f2e0207b82de01a155cceb17a0d86ab2
tttamaki1 <130736956+tttamaki1@users.noreply.github.com>
2023-06-08 18:06:32 +0900

    1

--- app/javascript/packs/map_resize.js
+++ app/javascript/packs/map_resize.js
@@ -1,38 +1,41 @@
 document.addEventListener('turbolinks:load', function() {
-    const generatedPlanContainer = document.querySelector('.generated-plan-container');
-    const rightSideContainer = document.querySelector('.right-side-container'); 
-    const mapContainer = document.querySelector('.map-container'); 
-    const indexContainer = document.querySelector('.container-index'); 
+
+  if (document.body.classList.contains('.container-index')) {
+      const generatedPlanContainer = document.querySelector('.generated-plan-container');
+      const rightSideContainer = document.querySelector('.right-side-container'); 
+      const mapContainer = document.querySelector('.map-container'); 
+      const indexContainer = document.querySelector('.container-index'); 
+      
+      function adjustWidth() {
+        const viewportWidth = window.innerWidth;
+        const viewportHeight = window.innerHeight;
+        let calculatedWidth;
+
+        indexContainer.style.width = `${viewportWidth}px`;
+
+      //   console.log(`viewportWidth:${viewportWidth} viewportHeight:${viewportHeight}`)
+        if (viewportWidth >= 1350) {
+          generatedPlanContainer.style.width = `${viewportWidth * 0.4}px`;
+          calculatedWidth = viewportWidth - (viewportWidth * 0.4) - 400;
+          
+        } else {
+          calculatedWidth = viewportWidth - generatedPlanContainer.style.width - 400;
+        }
+
+        calculatedHeight = viewportHeight - 220; // 62 + 50 + 50 + 40 + 18 = 220
+        rightSideContainer.style.height = `${calculatedHeight}px`;
+
+        mapContainer.style.width = `${calculatedWidth - 40}px`;
+        rightSideContainer.style.width = `${calculatedWidth}px`;
     
-    function adjustWidth() {
-      const viewportWidth = window.innerWidth;
-      const viewportHeight = window.innerHeight;
-      let calculatedWidth;
-
-      indexContainer.style.width = `${viewportWidth}px`;
-
-    //   console.log(`viewportWidth:${viewportWidth} viewportHeight:${viewportHeight}`)
-      if (viewportWidth >= 1350) {
-        generatedPlanContainer.style.width = `${viewportWidth * 0.4}px`;
-        calculatedWidth = viewportWidth - (viewportWidth * 0.4) - 400;
-        
-      } else {
-        calculatedWidth = viewportWidth - generatedPlanContainer.style.width - 400;
+      //   console.log(rightSideContainer.style.width);
       }
-
-      calculatedHeight = viewportHeight - 220; // 62 + 50 + 50 + 40 + 18 = 220
-      rightSideContainer.style.height = `${calculatedHeight}px`;
-
-      mapContainer.style.width = `${calculatedWidth - 40}px`;
-      rightSideContainer.style.width = `${calculatedWidth}px`;
-  
-    //   console.log(rightSideContainer.style.width);
-    }
-  
-    // ブラウザの幅が変わった時に実行
-    window.addEventListener('resize', adjustWidth);
-  
-    // 初期表示時に一度実行
-    adjustWidth();
+    
+      // ブラウザの幅が変わった時に実行
+      window.addEventListener('resize', adjustWidth);
+    
+      // 初期表示時に一度実行
+      adjustWidth();
+    }  
   });
   
